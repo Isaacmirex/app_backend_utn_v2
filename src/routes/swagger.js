6849fe5e1fd4c4ef708c7d07fc595e4f9db40439
unknown <isaacmirex@gmail.com>
2024-01-09 11:19:18 -0500

    Actualizacion rutas

--- src/routes/swagger.js
+++ src/routes/swagger.js
@@ -41,18 +41,18 @@ const swaggerDocs  = (app, puerto) => {
         }
     `;
 
-    app.use('/utnbackend/v2/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs, {
+    app.use('/utnbackend/v3/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs, {
         explorer: true,
         customCss,
     }));
 
     // Ruta para obtener el archivo JSON de la especificación Swagger
-    app.get('/utnbackend/v2/docs.json', (req, res) => {
+    app.get('/utnbackend/v3/docs.json', (req, res) => {
         res.setHeader('Content-Type', 'application/json');
         res.send(swaggerSpecs);
     });
 
-    console.log(`La documentación Swagger está disponible en http://localhost:${puerto}/utnbackend/v2/docs`);
+    console.log(`La documentación Swagger está disponible en http://localhost:${puerto}/utnbackend/v3/docs`);
 };
 
 export { swaggerDocs  };
